533bfb169740a28ceb376b1ef2db6701e897513e
Tim Saunders <tim@carmendata.co.uk>
2017-07-26 18:33:23 +0100

    Notes-node app first commit

---
+++ notes-node/app.js
@@ -0,0 +1,15 @@
+console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+console.log( user );
+
+fs.appendFile( 'greetings.txt', `Hello ${user.username}`, function(err){
+    if(err){
+        console.log( 'unable to write to file' );
+    }
+});
+

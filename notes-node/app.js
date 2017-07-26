b4f3c5ab2d4266da2d3a0ac1a4812e90d0a5e694
Tim Saunders <tim@carmendata.co.uk>
2017-07-26 18:57:57 +0100

    Added lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,11 +3,19 @@ console.log('starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
-
-var res = notes.add(5,3);
-console.log( `Result: ${res}` );
+const _ = require('lodash');
 
 
+console.log( _.isString(true) );
+console.log( _.isString(7) );
+console.log( _.isString('Tim') );
+
+var filteredArray = _.uniq(['Tim', 7, 'Tim', 1, 2, 3, 4, 7, 'Tim']);
+console.log( filteredArray );
+
+
+// var res = notes.add(5,3);
+// console.log( `Result: ${res}` );
 
 
 // var user = os.userInfo();

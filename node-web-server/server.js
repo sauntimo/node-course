ac4d7e8b923ed50c430be0c97d82fd5587cb53eb
Tim Saunders <tim@sauntimo.org>
2017-08-01 00:54:47 +0100

    node-web-sever: get ready to deploy to heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const hbs = require( 'hbs' );
 const fs = require( 'fs' );
 
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials( __dirname + '/views/partials' );
 app.set( 'view engine', 'hbs' );
@@ -56,8 +57,6 @@ app.get( '/bad', ( req, res ) => {
   });
 });
 
-const PORT = 3000;
-
-app.listen( PORT, () => {
-  console.log( `Server is listening on port ${PORT}` )
+app.listen( port, () => {
+  console.log( `Server is listening on port ${port}` )
 });

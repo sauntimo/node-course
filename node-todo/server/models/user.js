5ae7b3c4fbe20eb3934f5389052f3ae4858ae2dd
Tim Saunders <tim@sauntimo.org>
2017-08-12 14:25:17 +0100

    node-todo: implemented json web tokens on POST /users route

--- node-todo/server/models/user.js
+++ node-todo/server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     required: true,
     type: String,
@@ -10,7 +12,8 @@ var User = mongoose.model('User', {
     unique: true,
     validate: {
       validator: validator.isEmail,
-      message: '{VALUE} is not a valid email.'
+      message: '{VALUE} is not a valid email.',
+      isAsync: false
     }
   },
   password: {
@@ -29,5 +32,26 @@ var User = mongoose.model('User', {
     }
   }]
 });
+
+UserSchema.methods.toJSON = function(){
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+// instance methods. Uses normal fn keyword, not arrow fn
+// so that we can use this keyword
+UserSchema.methods.generateAuthToken = function(){
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123' ).toString();
+  user.tokens.push({access,token});
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- node-todo/server/server.js
+++ node-todo/server/server.js
@@ -97,12 +97,15 @@ app.patch('/todos/:id', (req, res) => {
 
 app.post( '/users', ( req, res ) => {
   var body = _.pick(req.body, ['email', 'password']);
-
   var user = new User(body);
-
-  user.save().then( (user) => {
-    res.send(user);
-  }).catch((e) => {
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res
+      .header('x-auth', token)
+      .send(user);
+  })
+  .catch((e) => {
     res.status(400).send(e);
   });
 });
